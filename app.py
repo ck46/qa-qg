4458a4d6ef624a996cb8bea6a509557cdca29f8e
ck46 <prof.chakas@gmail.com>
2021-12-14 12:39:10 +0200

    Added text area and sidebar

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 import streamlit as st
 
-x = st.slider('Select a value')
-st.write(x, 'squared is', x * x)
+# Add a model selector to the sidebar
+model = st.sidebar.selectbox(
+    'Select Model',
+    ('t5-base-squad-qa-qg', 't5-small-squad-qa-qg', 't5-base-hotpot-qa-qg', 't5-small-hotpot-qa-qg')
+)
+
+st.header('Question-Answer Generation')
+st.write(f'Model in use: {model}')
+
+txt = st.text_area('Text for context')
+
+
+if len(txt) >= 1:
+    autocards = []
+else:
+    autocards = []
+
+st.header('Generated question and answers')
+st.write(autocards)
 
 
